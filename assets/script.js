0dd799c33669acf27a871cedc0d3df3397bc5832
FectDay <141957977+FectDay@users.noreply.github.com>
2025-05-02 12:04:11 +0300

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Пример, как можно сделать скрипт для плавного прокручивания к якорям
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
